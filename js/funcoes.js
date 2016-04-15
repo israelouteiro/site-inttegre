2c023ed4e9b809fd02c753d8190bb185ffa8d36f
Claudio Roberto <claudiogoncalvesrp@gmail.com>
2016-04-14 21:03:52 -0300

    Rolagem da home

--- js/funcoes.js
+++ js/funcoes.js
@@ -63,6 +63,18 @@
         });
     });
 
+$(document).ready(function() {
+  $("#navbar a[href^='#']").on("click", function(e) {
+    e.preventDefault(); 
+   
+    
+
+    var hash = this.hash;
+    $("html, body").animate( {
+      scrollTop: $(hash).offset().top
+    }, 900);
+  });
+});
 
 
 
